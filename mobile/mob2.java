07b2741248a20f73f61736c927a06127f4329b3e
VeeraVDK <veerakarsudarvdk@gmail.com>
2025-12-05 11:08:12 +0530

    take photo

---
+++ mobile/mob2.java
@@ -0,0 +1,15 @@
+package mobile;
+
+public class mob2 {
+	public void takephoto()
+	{
+		System.out.println("take photo");
+		
+	}
+	public static void main(String[]args)
+	{
+		mob2 obj=new mob2();
+		obj.takephoto();
+	}
+
+}
